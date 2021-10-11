b9dc90805036a20d1a35293d037a006bd6d15d47
barelampagos <barelampagos@gmail.com>
2021-10-10 18:04:33 -0700

    Adding static API route to fetch from data.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import { server } from '../config';
+
 import Head from 'next/head';
 import ArticleList from '../components/ArticleList';
 
@@ -13,10 +15,21 @@ export default function Home({ articles }) {
 	);
 }
 
+// export const getStaticProps = async () => {
+// 	const res = await fetch(
+// 		`https://jsonplaceholder.typicode.com/posts?_limit=6`
+// 	);
+// 	const articles = await res.json();
+
+// 	return {
+// 		props: {
+// 			articles
+// 		}
+// 	};
+// };
+
 export const getStaticProps = async () => {
-	const res = await fetch(
-		`https://jsonplaceholder.typicode.com/posts?_limit=6`
-	);
+	const res = await fetch(`${server}/api/articles/`);
 	const articles = await res.json();
 
 	return {
